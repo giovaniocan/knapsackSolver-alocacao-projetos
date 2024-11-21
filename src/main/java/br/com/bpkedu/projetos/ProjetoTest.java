91b7d60582e766b1e18fd3fbb58930b0e5e854eb
giovaniocan <giovaniocan@gmail.com>
2024-11-21 20:01:10 -0300

    added random function

--- src/main/java/br/com/bpkedu/projetos/ProjetoTest.java
+++ src/main/java/br/com/bpkedu/projetos/ProjetoTest.java
@@ -2,23 +2,25 @@ package br.com.bpkedu.projetos;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class ProjetoTest {
     public static void main(String[] args) {
         // gerando o orcamento e quantos projetos tem
-        Double valorDisponivel = 300.0;
-        int numberOfProjects = 10;
+        Double valorDisponivel = 500.0;
+        int numberOfProjects = 4;
 
         // gerando os projetos meio aleatorios.
-       // List<Projeto> projetos = generateRandomProjects(numberOfProjects, 100.0, 500.0, 200.0, 1000.0); DEPOIS IMPLEMENTO ISSO DAQUI
+       List<Projeto> projetos = generateRandomProjects(numberOfProjects, 100.0, 250.0, 200.0, 1000.0);
 
-        // criando os projetos
+        /*// criando os projetos
         List<Projeto> projetos = new ArrayList<>();
         projetos.add(new Projeto("Projeto 1", 100.0, 200.0));
         projetos.add(new Projeto("Projeto 2", 50.0, 300.0));
         projetos.add(new Projeto("Projeto 3", 200.0, 400.0));
         projetos.add(new Projeto("Projeto 4", 75.0, 300.0));
         projetos.add(new Projeto("Projeto 5", 300.0, 250.0));
+        projetos.add(new Projeto("Projeto 6", 300.0, 2000.0));*/
 
 
         //mostrando todos os projetos disponiveis.
@@ -38,5 +40,18 @@ public class ProjetoTest {
         }
         System.out.printf("Investimento total: %.2f%n", melhorEstado.getTotalInvestimento());
         System.out.printf("Retorno total: %.2f%n", melhorEstado.getTotalRetorno());
+
     }
+         private static List<Projeto> generateRandomProjects(int count, double minInvestimento, double maxInvestimento, double minRetorno, double maxRetorno) {
+            List<Projeto> projetosRandom = new ArrayList<>();
+            Random random = new Random();
+
+            for (int i = 1; i <= count; i++) {
+                double investimento = minInvestimento + (maxInvestimento - minInvestimento) * random.nextDouble();
+                double retorno = minRetorno + (maxRetorno - minRetorno) * random.nextDouble();
+                projetosRandom.add(new Projeto("Projeto " + i, investimento, retorno));
+            }
+
+            return projetosRandom;
+            }
 }
